2456b77ce31f61b16ec2976801a1fa56e991a609
yangzhenkun <1334036616@qq.com>
2018-12-21 10:26:32 +0800

    增加word模版替换

--- src/main/java/file/ExportUtil.java
+++ src/main/java/file/ExportUtil.java
@@ -5,11 +5,16 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.usermodel.Range;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author yangzhenkun
@@ -67,6 +72,30 @@ public class ExportUtil {
             e.printStackTrace();
         }
 
+    }
+
+
+    public static void wordTemplateReplace(Map<String, String> data) {
+
+        try {
+            FileInputStream fis = new FileInputStream(new File("src/main/resources/template.doc"));
+            HWPFDocument hdt = new HWPFDocument(fis);
+            Range range = hdt.getRange();
+
+            data.forEach((k, v) -> {
+                String nk = "{" + k + "}";
+                range.replaceText(nk, v);
+            });
+
+            hdt.write(new File("src/main/resources/new.doc"));
+            hdt.close();
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
